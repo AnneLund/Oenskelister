a63e59dbea21274fc7bccddca8b2cba28503bf2f
AnneLund <89774650+AnneLund@users.noreply.github.com>
2021-12-07 20:37:16 +0100

    ...

--- script/script.js
+++ script/script.js
@@ -5,28 +5,28 @@ h1.addEventListener("input", function () {
   this.setAttribute("data-heading", this.innerText);
 });
 
-// window.localStorage.clear();
+window.localStorage.clear();
 
 //REBECCAS ØNSKELISTE
 
-function removeitemarraygirl() {
+function strikeitemarraygirl() {
   // giv dit input felt id 'ritem' eller andet og så ændre det neden under
   x = document.getElementById("pritem").value;
   // her ændre du det ønske på din list som du taster ind
-  document.getElementById("pi" + x).innerHTML = "";
+  document.getElementById("pi" + x).style.textDecorationLine = "line-through";
   // her sletter den alt det gamle du har gemt, for ikke at gemme tingende oven i hinanden
   window.localStorage.removeItem("pwish");
   // dette er din ønske list items tilføj flere men forsøge at beholde id strukturen
-  let pi0 = document.getElementById("pi0").innerHTML; //ønske 1
-  let pi1 = document.getElementById("pi1").innerHTML; //ønske 2
-  let pi2 = document.getElementById("pi2").innerHTML; //ønske 3
-  let pi3 = document.getElementById("pi3").innerHTML; //ønske 4
-  let pi4 = document.getElementById("pi4").innerHTML; //ønske 5
-  let pi5 = document.getElementById("pi5").innerHTML; //ønske 6
-  let pi6 = document.getElementById("pi6").innerHTML; //ønske 7
-  let pi7 = document.getElementById("pi7").innerHTML; //ønske 8
-  let pi8 = document.getElementById("pi8").innerHTML; //ønske 9
-  let pi9 = document.getElementById("pi9").innerHTML;
+  let pi0 = document.getElementById("pi0").style.textDecorationLine; //ønske 1
+  let pi1 = document.getElementById("pi1").style.textDecorationLine; //ønske 2
+  let pi2 = document.getElementById("pi2").style.textDecorationLine; //ønske 3
+  let pi3 = document.getElementById("pi3").style.textDecorationLine; //ønske 4
+  let pi4 = document.getElementById("pi4").style.textDecorationLine; //ønske 5
+  let pi5 = document.getElementById("pi5").style.textDecorationLine; //ønske 6
+  let pi6 = document.getElementById("pi6").style.textDecorationLine; //ønske 7
+  let pi7 = document.getElementById("pi7").style.textDecorationLine; //ønske 8
+  let pi8 = document.getElementById("pi8").style.textDecorationLine; //ønske 9
+  let pi9 = document.getElementById("pi9").style.textDecorationLine;
   // tilføj de extra items id i arrayet neden for
   let pwish = [pi0, pi1, pi2, pi3, pi4, pi5, pi6, pi7, pi8, pi9]; // i dette array står din ønsker
   // nu gemmer vi så listen i local storage
@@ -37,28 +37,29 @@ let pige_wish_storage = JSON.parse(window.localStorage.getItem("pwish"));
 // her er der et for loop der går alle din ønsker igennem og tjekker om der nogen der er blevet fjerne fra listen
 for (i = 0; i < pige_wish_storage.length; i++) {
   // hvor den så fjerner de ønsker på hjemmesiden
-  document.getElementById("pi" + [i]).innerHTML = pige_wish_storage[i];
+  document.getElementById("pi" + [i]).style.textDecorationLine =
+    pige_wish_storage[i];
 }
 
 //VALDEMARS ØNSKELISTE
 
-function removeitemarrayboy() {
+function strikeitemarrayboy() {
   // giv dit input felt id 'ritem' eller andet og så ændre det neden under
   x = document.getElementById("dritem").value;
   // her ændre du det ønske på din list som du taster ind
-  document.getElementById("di" + x).innerHTML = "";
+  document.getElementById("di" + x).style.textDecorationLine = "line-through";
   // her sletter den alt det gamle du har gemt, for ikke at gemme tingende oven i hinanden
   window.localStorage.removeItem("dwish");
   // dette er din ønske list items tilføj flere men forsøge at beholde id strukturen
-  let di0 = document.getElementById("di0").innerHTML; //ønske 1
-  let di1 = document.getElementById("di1").innerHTML; //ønske 2
-  let di2 = document.getElementById("di2").innerHTML; //ønske 3
-  let di3 = document.getElementById("di3").innerHTML; //ønske 4
-  let di4 = document.getElementById("di4").innerHTML; //ønske 5
-  let di5 = document.getElementById("di5").innerHTML; //ønske 6
-  let di6 = document.getElementById("di6").innerHTML; //ønske 7
-  let di7 = document.getElementById("di7").innerHTML; //ønske 8
-  let di8 = document.getElementById("di8").innerHTML; //ønske 9
+  let di0 = document.getElementById("di0").style.textDecorationLine; //ønske 1
+  let di1 = document.getElementById("di1").style.textDecorationLine; //ønske 2
+  let di2 = document.getElementById("di2").style.textDecorationLine; //ønske 3
+  let di3 = document.getElementById("di3").style.textDecorationLine; //ønske 4
+  let di4 = document.getElementById("di4").style.textDecorationLine; //ønske 5
+  let di5 = document.getElementById("di5").style.textDecorationLine; //ønske 6
+  let di6 = document.getElementById("di6").style.textDecorationLine; //ønske 7
+  let di7 = document.getElementById("di7").style.textDecorationLine; //ønske 8
+  let di8 = document.getElementById("di8").style.textDecorationLine; //ønske 9
   // tilføj de extra items id i arrayet neden for
   let dwish = [di0, di1, di2, di3, di4, di5, di6, di7, di8]; // i dette array står din ønsker
   // nu gemmer vi så listen i local storage
@@ -69,5 +70,6 @@ let dreng_wish_storage = JSON.parse(window.localStorage.getItem("dwish"));
 // her er der et for loop der går alle din ønsker igennem og tjekker om der nogen der er blevet fjerne fra listen
 for (i = 0; i < dreng_wish_storage.length; i++) {
   // hvor den så fjerner de ønsker på hjemmesiden
-  document.getElementById("di" + [i]).innerHTML = dreng_wish_storage[i];
+  document.getElementById("di" + [i]).style.textDecorationLine =
+    dreng_wish_storage[i];
 }
